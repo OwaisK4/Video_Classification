e7d8a33fc00741ff93668061e5fe146d646bc79e
hassaangatta <hassaangatta@gmail.com>
2024-06-06 00:20:07 +0500

    Updated scrapper 1

--- Web_Scrapping/First_Version.py
+++ Web_Scrapping/First_Version.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def googleRating(location):
@@ -13,15 +15,20 @@ def googleRating(location):
     options.add_argument("--headless")
     url = "https://www.google.com/maps"
     driver = webdriver.Firefox(options=options)
-    driver.get(url)
-    time.sleep(6)
-    search = driver.find_element(By.ID, 'searchboxinput')
-    search.send_keys(location)
-    time.sleep(6)
-    search = driver.find_element(By.ID, 'searchbox-searchbutton')
-    search.click()
-    time.sleep(6)
     try:
+        driver.get(url)
+        element_locator = (By.ID,'searchboxinput')
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(element_locator)
+        )
+        search = driver.find_element(By.ID, 'searchboxinput')
+        search.send_keys(location)
+        search = driver.find_element(By.ID, 'searchbox-searchbutton')
+        search.click()
+        element_locator = (By.CLASS_NAME,'F7nice')
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(element_locator)
+        )
         ratingDiv = driver.find_element(By.CLASS_NAME,'F7nice')
         # print(ratingDiv.text)
         temp = ratingDiv.text
@@ -30,11 +37,8 @@ def googleRating(location):
         temp = temp[1].split('(')
         temp = temp[1].split(')')
         res['no of people'] = temp[0]   
-    except: 
-        # print("Not found")
-        pass
-
-    time.sleep(1)
+    except:
+        print('No rating found on google or network issue')
     driver.quit()
     return res
 
@@ -44,21 +48,24 @@ def description(location):
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
-    driver.get(url)
-    time.sleep(6)
-    search = driver.find_element(By.ID, 'APjFqb')
-    search.send_keys('give me a short description of ' + location)
-    search.send_keys(Keys.ENTER)
-    time.sleep(6)
     try:
+        driver.get(url)
+        element_locator = (By.ID,'APjFqb')
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(element_locator)
+        )
+        search = driver.find_element(By.ID, 'APjFqb')
+        search.send_keys('give me a short description of ' + location)
+        search.send_keys(Keys.ENTER)
+        element_locator = (By.CLASS_NAME,'hgKElc')
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(element_locator)
+        )
         description = driver.find_element(By.CLASS_NAME,'hgKElc')
         # print(description.text)
         res = description.text   
-    except: 
-        # print("Not found")
-        pass
-
-    time.sleep(1)
+    except:
+        print('No Description on google or network issue')
     driver.quit()
     return res
 
@@ -71,13 +78,19 @@ def tripadvisorRating(location):
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
-    driver.get(url)
-    time.sleep(6)
-    search = driver.find_element(By.ID, 'APjFqb')
-    search.send_keys('what is the rating of ' + location + ' by tripadvisor?')
-    search.send_keys(Keys.ENTER)
-    time.sleep(6)
     try:
+        driver.get(url)
+        element_locator = (By.ID,'APjFqb')
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(element_locator)
+        )
+        search = driver.find_element(By.ID, 'APjFqb')
+        search.send_keys(location + ' tripadvisor')
+        search.send_keys(Keys.ENTER)
+        element_locator = (By.XPATH,"//*[@id='rso']/div[1]/div/div/div/div[4]/div")
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(element_locator)
+        )
         rating = driver.find_element(By.XPATH,"//*[@id='rso']/div[1]/div/div/div/div[4]/div")
         # print(rating.text)
         temp = rating.text
@@ -85,10 +98,8 @@ def tripadvisorRating(location):
         res['rating'] = temp[1]
         res['no of people'] = temp[3]   
     except: 
-        # print("Not found")
+        print('No rating found on tripadvisor or network issue')
         pass
-
-    time.sleep(1)
     driver.quit()
     return res
 
@@ -101,7 +112,29 @@ def getRatingAndDescription(location):
     ans['google'] = googleRating(location)
     ans['description'] = description(location)
     ans['tripadvisor'] = tripadvisorRating(location)
-    print(ans)
+    # print(ans)
     return ans
 
-getRatingAndDescription('cn tower')
+locations = [
+    "Chinatown", "art gallery", "New York", "city halls", "contemporary Art Museum", "building", 
+    "fast food joint", "bike lanes", "Canada", "town", "castle", "corridors", "LA", "Distillery District", 
+    "subway", "harbor", "Vancouver", "U.S.", "museums", "Toronto", "Queen St. West", "historic homes", 
+    "history museums", "restaurant", "subway line", "outside", "galleries", "ATM", "Presto", "OK", 
+    "Chicago", "crate city", "Koreatown", "neighborhood", "city", "metro", "Casa Loma", "Union Station", 
+    "Cabbagetown", "shops", "Greektown", "hallways", "waterfront", "downtown", "hotel", "sand tower", 
+    "working area", "valet", "boutiques", "home", "Kensington Market", "Toronto, Canada", "bike lane", 
+    "dry land", "boardwalk", "CN Tower", "jungle", "malls", "Hockey Hall of Fame", "kiosks", "ocean", 
+    "Yorkville", "St", "block", "residential area", "Roger Center", "Ripley's Aquarium", "Toronto Railway Museum", 
+    "Union", "lake", "Film Cafe", "buildings", "halls", "hiking trails", "Nathan Phillips Square", "stores", 
+    "New York City", "dogs off leash area", "Bang Bang Ice Cream Shop", "elevator", "city limits", "City Hall", 
+    "Film Cafe", "Saint Lawrence Market", "Kensington Market", "Eaton Center", "gift shop", "Italy", "restaurants", 
+    "rooftop bar", "Montreal", "Trophy Hall", "observation deck", "street", "Canadas", "restaurant", "temple", 
+    "High Park", "sports facilities", "aisles", "stalls", "zoo", "city", "gardens", "Beaches", "U.S.", 
+    "coffee shops", "closet", "America", "coffee shops", "Kensington", "overseas", "townhouses", "restaurants", 
+    "neighborhood", "Boston", "Cape", "alleyways", "shower", "balcony room", "Hotel Toronto", "Japanese", 
+    "little bar area", "Thrift stores", "Subway", "underground", "beauty store", "park area", "coffee shop", 
+    "Trinity Melwood", "Toronto Light Rd.", "W Toronto", "Nut Bar", "bars", "Times Square", "park", 
+    "hotel room", "Korean", "shopping area", "Wilford"
+]
+
+getRatingAndDescription("Toronto Railway Museum toronto")
---
+++ Web_Scrapping/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+Bang Bang Ice Cream Shop toronto
